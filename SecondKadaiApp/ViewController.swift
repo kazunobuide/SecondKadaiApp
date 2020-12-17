aa56067e5fd5419fe11575e4b028e5cb688b88bd
kazunobu.ide <idekazunobu@gmail.com>
2020-12-17 18:42:57 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 井手　和宣 on 2020/12/17.
+//  Copyright © 2020 kazunobu.ide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
